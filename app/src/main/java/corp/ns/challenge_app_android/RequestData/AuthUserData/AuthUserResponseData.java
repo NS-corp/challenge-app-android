5c2dfd3d5b9b1b015de346cd38ec282e3622f65f
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-04-04 00:01:44 +0300

    Update AuthUserResponseData
    - Change User info name

--- app/src/main/java/corp/ns/challenge_app_android/RequestData/AuthUserData/AuthUserResponseData.java
+++ app/src/main/java/corp/ns/challenge_app_android/RequestData/AuthUserData/AuthUserResponseData.java
@@ -4,15 +4,20 @@ import android.os.Parcel;
 
 import corp.ns.challenge_app_android.RequestData.Data.UserInfo;
 import corp.ns.challenge_app_android.RequestData.DefaultData.DefaultResponseData;
-import lombok.Getter;
-import lombok.Setter;
 
 public class AuthUserResponseData extends DefaultResponseData {
 
     /* Request fields */
 
-    @Getter @Setter
-    UserInfo userInfo;
+    private UserInfo uinf;
+
+    public UserInfo getUserInfo() {
+        return uinf;
+    }
+
+    public void setUserInfo(UserInfo uInf) {
+        this.uinf = uInf;
+    }
 
     @Override
     public int describeContents() {
@@ -22,17 +27,17 @@ public class AuthUserResponseData extends DefaultResponseData {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         super.writeToParcel(dest, flags);
-        dest.writeParcelable(this.userInfo, flags);
+        dest.writeParcelable(this.uinf, flags);
     }
 
     public AuthUserResponseData(boolean requestStatus, UserInfo userInfo) {
         super(requestStatus);
-        this.userInfo = userInfo;
+        this.uinf = userInfo;
     }
 
     protected AuthUserResponseData(Parcel in) {
         super(in);
-        this.userInfo = in.readParcelable(UserInfo.class.getClassLoader());
+        this.uinf = in.readParcelable(UserInfo.class.getClassLoader());
     }
 
     public static final Creator<AuthUserResponseData> CREATOR = new Creator<AuthUserResponseData>() {
